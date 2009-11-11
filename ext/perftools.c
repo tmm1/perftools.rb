6f00537d5332660e9bd3120ced1518c3cff79753
Aman Gupta <aman@tmm1.net>
2009-11-11 15:36:49 -0800

    Ignore all #send

--- ext/perftools.c
+++ ext/perftools.c
@@ -11,7 +11,7 @@ static VALUE I__send__;
 static VALUE Isend;
 
 #define SAVE_FRAME() \
-  if (method && method != I__send__ && !(method == Isend && klass == rb_cObject)) { \
+  if (method && method != I__send__ && method != Isend) { \
     if (self && FL_TEST(klass, FL_SINGLETON) && (BUILTIN_TYPE(self) == T_CLASS || BUILTIN_TYPE(self) == T_MODULE)) \
       result[depth++] = (void*) self; \
     else \
