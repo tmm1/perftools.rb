7da4dfbfa8833497f41d2a5aa3b9feb8e78fa63e
coderrr <coderrr.contact@gmail.com>
2010-04-09 17:11:02 -0400

    fix for freebsd

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -80,7 +80,7 @@ def add_define(name)
 end
 
 case RUBY_PLATFORM
-when /darwin/, /linux/
+when /darwin/, /linux/, /freebsd/
   CONFIG['LDSHARED'] = "$(CXX) " + CONFIG['LDSHARED'].split[1..-1].join(' ')
 end
 
